a30a25e701c72a8b6d1356ab4271cc601ea0159c
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-03 01:57:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,26 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(path)
+  result = {
+    :get_meaning => {},      
+    :get_emoticon => {}
+  }
+  emoticons = YAML.load_file(path)
+  emoticons.each do |key, value|  # value[1]  
+     result[:get_meaning][value[1]] = key 
+     result[:get_emoticon][value[0]] = value[1]
+    # binding.pry   
+  end 
+  
+  return result
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
+def get_japanese_emoticon(path, emoticon)
+   hash = load_library(path)
+    binding.pry
+end 
 
 def get_english_meaning
   # code goes here
